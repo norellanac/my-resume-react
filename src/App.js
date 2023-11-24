d512c26284f8c4d8fa2a98e46d90b876e1b91b9a
Andrea Calderon <alvanesc@gmail.com>
2023-11-24 07:45:08 -0600

    Creacion de seccion 'Work Experience'

---
+++ src/App.js
@@ -0,0 +1,47 @@
+import React, { useState } from "react";
+import i18n from "./utils/i18n";
+import { Button } from 'react-bootstrap';
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import './App.css';
+import WorkExperience from './workExperience';
+
+
+
+
+
+function App() {
+  const [currentLanguage, setCurrentLanguage] = useState("en");
+
+  const changeLanguage = (language) => {
+    i18n.locale = language;
+    setCurrentLanguage(language);
+  };
+  return (
+    <div className="App">
+          <div>
+            <h1>{i18n.t("hello")}</h1>
+            <div>
+              <Button variant="primary" onClick={() => changeLanguage("en")}>English!</Button>
+                <button onClick={() => changeLanguage("es")}>Spanish</button>
+            </div>
+          </div>
+        
+      <Container>
+        <Row>
+        
+          <Col > 
+            <div style={{color:"#1C8EB5",fontSize:"23px", textAlign:"left"}}>{i18n.t("experience-title")}</div>
+            < WorkExperience />
+          
+          </Col>
+
+        </Row>
+      
+      </Container>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/workExperience.js
@@ -0,0 +1,54 @@
+import React from 'react';
+import Card from 'react-bootstrap/Card';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import i18n from "./utils/i18n";
+
+function WorkExperience() {
+  return (
+    <Row>
+      <Col>
+        <div>
+          <Card style={{ width: '30rem',textAlign:"right",border:0 }}>
+            <Card.Body>
+              <Card.Title style={{color:"#222"}}>{i18n.t("workExperience[0].title")}</Card.Title>
+              <Card.Text style={{color:"#797979",fontFamily:"Prata"}}>
+                -{i18n.t("workExperience[0].experience")}, -{i18n.t("workExperience[1].summary")}, {i18n.t("workExperience[0].start_date")} - {i18n.t("workExperience[0].end_date")}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card style={{ width: '30rem',textAlign:"right",border:0 }}>
+            <Card.Body>
+              <Card.Title style={{color:"#222"}}>{i18n.t("workExperience[1].title")}</Card.Title>
+              <Card.Text style={{color:"#797979" ,fontFamily:"Prata"}}>
+                -{i18n.t("workExperience[1].experience")}, -{i18n.t("workExperience[1].summary")}, {i18n.t("workExperience[1].start_date")} - {i18n.t("workExperience[1].end_date")}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card style={{ width: '30rem',textAlign:"right",border:0 }}>
+            <Card.Body>
+              <Card.Title style={{color:"#222"}}>{i18n.t("workExperience[2].title")}</Card.Title>
+              <Card.Text style={{color:"#797979" ,fontFamily:"Prata"}}>
+                -{i18n.t("workExperience[2].experience")}, -{i18n.t("workExperience[1].summary")}, {i18n.t("workExperience[1].start_date")} - {i18n.t("workExperience[1].end_date")}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+
+          <Card style={{ width: '30rem',textAlign:"right",border:0 }}>
+            <Card.Body>
+              <Card.Title style={{color:"#222"}}>{i18n.t("workExperience[3].title")}</Card.Title>
+              <Card.Text style={{color:"#797979" ,fontFamily:"Prata"}}>
+                -{i18n.t("workExperience[3].experience")}, -{i18n.t("workExperience[1].summary")}, {i18n.t("workExperience[1].start_date")} - {i18n.t("workExperience[1].end_date")}
+              </Card.Text>
+            </Card.Body>
+          </Card>
+         
+        </div>
+      </Col>
+    </Row>
+  );
+}
+
+export {WorkExperience};
